152eb459a5146500cdd8beb2d8155543d914a20b
Dalete Pokrywiesky <95585407+DaleteB@users.noreply.github.com>
2022-05-09 19:29:14 +0100

    defined columns

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props){
                     <div className="row">
                         {props.photos.map(function(photo, index){
                             return (
-                            <div className="col-4" key={index}>
+                            <div className="col-6" key={index}>
                                 <a href={photo.src.original} target="_blank" rel="noopener noreferrer">
                                     <img src={photo.src.landscape} alt={photo.alt} className="img-fluid"/>
                                 </a>
